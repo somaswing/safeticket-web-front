def9bdfd335bdf485977c9f06919fcccf913993c
wonjongoh94 <owen@getliner.com>
2019-11-02 15:13:52 +0900

    route 정리

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import MainView from '@/views/MainView';
+import APIView from '@/views/APIView';
+import DemoView from '@/views/DemoView';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'MainView',
+      component: MainView,
+    },
+    {
+      path: '/api',
+      name: 'APIView',
+      component: APIView,
+    },
+    {
+      path: '/demo',
+      name: 'DemoView',
+      component: DemoView,
     },
   ],
 });
---
+++ src/views/MainView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="api">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'APIView',
+  data() {
+    return {
+      msg: 'This page is for Main',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
