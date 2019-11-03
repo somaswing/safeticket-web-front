5cfe40f916519f50755130821fbd3f3f96e0ece4
wonjongoh94 <owen@getliner.com>
2019-11-03 19:38:09 +0900

    Tech,Team 컴포넌트 마크업

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,5 +1,29 @@
 <template>
   <div id="team">
+    <div class="title">Team</div>
+    <div class="members">
+      <div class="member">
+        <img src="@/assets/logo.png" alt="img">
+        <p class="member_name">김동준</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque blanditiis commodi consectetur deleniti
+          dignissimos distinctio fugit, illo labore libero natus nemo officia optio pariatur possimus quam qui,
+          voluptate. Architecto?</p>
+      </div>
+      <div class="member">
+        <img src="@/assets/logo.png" alt="img">
+        <p class="member_name">김문서</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque blanditiis commodi consectetur deleniti
+          dignissimos distinctio fugit, illo labore libero natus nemo officia optio pariatur possimus quam qui,
+          voluptate. Architecto?</p>
+      </div>
+      <div class="member">
+        <img src="@/assets/logo.png" alt="img">
+        <p class="member_name">오원종</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At atque blanditiis commodi consectetur deleniti
+          dignissimos distinctio fugit, illo labore libero natus nemo officia optio pariatur possimus quam qui,
+          voluptate. Architecto?</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,5 +41,21 @@ export default {
     width: 100%;
     height: 100vh;
     background-color: rgba(0, 255, 255, 0.3);
+  }
+  .title {
+    padding: 50px 0;
+    margin: 0 auto;
+    font-size: 48px;
+    font-weight: bold;
+  }
+  .members {
+    display: flex;
+  }
+  .member {
+    flex-grow: 1;
+    padding: 20px;
+  }
+  .member_name {
+    font-size: 28px;
   }
 </style>
--- src/components/Tech.vue
+++ src/components/Tech.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="tech">
+    <div class="blockchain">
+      <div class="tech_image">
+        <img src="@/assets/logo.png" alt="img">
+      </div>
+      <div class="explain">
+        <div class="tech_title">BlockChain</div>
+        <div class="tech_explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Ab accusantium ad at corporis, culpa cumque, dolore et harum hic in ipsam magnam
+          maxime numquam possimus quas quisquam sit. Magni, vitae.</div>
+      </div>
+    </div>
+    <div class="did">
+      <div class="explain">
+        <div class="tech_title">Distrubuted ID</div>
+        <div class="tech_explain">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Ab accusantium ad at corporis, culpa cumque, dolore et harum hic in ipsam magnam
+          maxime numquam possimus quas quisquam sit. Magni, vitae.</div>
+      </div>
+      <div class="tech_image">
+        <img src="@/assets/logo.png" alt="img">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,5 +39,28 @@ export default {
     width: 100%;
     height: 100vh;
     background-color: rgba(128, 0, 128, 0.3);
+  }
+  #tech > div {
+    display: flex;
+    padding: 10px 30px;
+  }
+  .tech_image {
+    min-width: 300px;
+  }
+  img {
+    width: 100%;
+    height: auto;
+  }
+  .explain {
+    padding: 30px;
+  }
+  .explain > div {
+    padding: 20px 0;
+  }
+  .tech_title {
+    font-size: 48px;
+  }
+  .tech_explain {
+    font-size: 24px;
   }
 </style>
