28038befe7393b8f3e3a7ef34ad16e6d3af636bb
wonjongoh94 <owen@getliner.com>
2019-11-15 01:55:38 +0900

    demo qrcode value 맞게 수정

--- src/components/QRcode.vue
+++ src/components/QRcode.vue
@@ -9,7 +9,7 @@ import QrcodeVue from 'qrcode.vue';
 export default {
   data() {
     return {
-      value: "{'attendee_id':'abc123','event_name':'IU콘서트'}",
+      value: "{'token':'$2b$12$lE.8WOd7SlrLNuK2hrnp3uginBvvsGExazM96TBWNdaxxSJg1jV8', 'email':'safeticket@saficket.com', 'venue': 'Olympic Stadium', 'event_name': 'ILOVEIU', 'event_date': '20191130', 'event_time': '19:00', 'payment_time': '20191121130513', 'ticket_issuer': 'interpark'}",
       size: 300,
     };
   },
